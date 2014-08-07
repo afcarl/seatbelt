d30920d6f18f90ddc4809db669398b41e5f640f9
Robert M Ochshorn <rmo@NUMM.ORG>
2014-08-07 16:17:10 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import distutils.core
+
+distutils.core.setup(
+    name='seatbelt',
+    version='0.0',
+    url='http://readmes.numm.org/seatbelt',
+    description='http/websocket database/server',
+    author='Robert M Ochshorn',
+    author_email='rmo@numm.org',
+    packages=['seatbelt'],
+    license='AGPL')
